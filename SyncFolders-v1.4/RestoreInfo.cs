b605d357dd25bba8a1d2990c3cc0422d10e5860c
NataljaNeumann <NataljaNeumann@gmx.de>
2024-12-26 22:08:41 +0100

    Added comment

--- SyncFolders-v1.4/RestoreInfo.cs
+++ SyncFolders-v1.4/RestoreInfo.cs
@@ -25,24 +25,48 @@ using System.Text;
 
 namespace SyncFolders
 {
+    /// <summary>
+    /// Object of this class provide information about single restores
+    /// </summary>
     class RestoreInfo
     {
+        /// <summary>
+        /// The position to write to
+        /// </summary>
         public long Position = 0;
+        /// <summary>
+        /// The block to write. (Actual length needs to be calculated outside, based on
+        /// position and file size
+        /// </summary>
         public Block Data = Block.GetBlock();
+        /// <summary>
+        /// Indicates that this is non-recoverable area, so the block is empty
+        /// to fill non-recoverable space.
+        /// </summary>
         public bool NotRecoverableArea;
 
+        /// <summary>
+        /// Constructs a new restore info
+        /// </summary>
         public RestoreInfo()
         {
         }
 
-        public RestoreInfo(long pos, Block data, bool notRecoverable)
+        /// <summary>
+        /// Constructs a new restore info
+        /// </summary>
+        /// <param name="nPos">The position for restoring</param>
+        /// <param name="oData">The data to write</param>
+        /// <param name="bNotRecoverable">Indicates that this is an empty block for non-recoverabl
+        /// area</param>
+        public RestoreInfo(long nPos, Block oData, bool bNotRecoverable)
         {
-            Position = pos;
-            // copy data to a new block, so the original can be reused
+            Position = nPos;
+            // copy oData to a new block, so the original can be reused
             Block b = Data;
             for (int i=b.Length-1;i>=0;--i)
-                b[i] = data[i];
-            NotRecoverableArea = notRecoverable;
+                b[i] = oData[i];
+            NotRecoverableArea = bNotRecoverable;
         }
     }
 }
